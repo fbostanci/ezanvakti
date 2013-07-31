44695f6e8fbd131cce6f3817330a2e63d733cc29
fbostanci <ironic@yaani.com>
2013-07-31 23:08:00 +0300

    betaya_hazirlik-2

---
+++ lib/mplayer_yonetici.bash
@@ -0,0 +1,12 @@
+
+
+function mplayer_calistir() {
+  local dinletilecek_oge="$1"
+  
+  rm -f /tmp/mplayer-$$.pipe 2>/dev/null
+  mkfifo /tmp/mplayer-$$.pipe
+  $MPLAYER -slave -input file=/tmp/mplayer-$$.pipe "${dinletilecek_oge}" 2>/dev/null; clear
+  rm -f /tmp/mplayer-$$.pipe 2>/dev/null
+}
+
+# vim: set ft=sh ts=2 sw=2 et:
