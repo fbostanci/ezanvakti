b67088a0bba66027ecdf6be8bb95ab912f2202d6
ironic <faopera@gmail.com>
2013-08-30 15:15:16 +0300

    betaya-hazirlik-11

--- lib/oynatici_duraklat.bash
+++ lib/oynatici_duraklat.bash
@@ -41,60 +41,60 @@ function oynatici_islem() {
 
   for oynatici in ${OYNATICILAR[@]}
   do
-    [ $(pgrep ${oynatici}) ] && {
-      if [ ${oynatici} = deadbeef ]
+    [[ -n $(pgrep ${oynatici}) ]] && {
+      if [[ ${oynatici} = deadbeef ]]
       then
           deadbeef --pause > /dev/null 2>&1
           DURDURULAN+=('deadbeef')
-      elif [ ${oynatici} = clementine ]
+      elif [[ ${oynatici} = clementine ]]
       then
           if qdbus_sorgu clementine;
           then
               clementine --pause > /dev/null 2>&1
               DURDURULAN+=('clementine')
           fi
-      elif [ ${oynatici} = amarok ]
+      elif [[ ${oynatici} = amarok ]]
       then
           if qdbus_sorgu amarok;
           then
               amarok --pause  > /dev/null 2>&1
               DURDURULAN+=('amarok')
           fi
-      elif [ ${oynatici} = rhythmbox ]
+      elif [[ ${oynatici} = rhythmbox ]]
       then
           rhythmbox-client --pause  > /dev/null 2>&1
           DURDURULAN+=('rhytmbox')
-      elif [ ${oynatici} = aqulung ]
+      elif [[ ${oynatici} = aqulung ]]
       then
           aqualung --pause > /dev/null 2>&1
           DURDURULAN+=('aqualung')
-      elif [ ${oynatici} = audacious ]
+      elif [[ ${oynatici} = audacious ]]
       then
           if dbus_sorgu audacious;
           then
               audacious --pause > /dev/null 2>&1
               DURDURULAN+=('audacious')
           fi
-      elif [ ${oynatici} = banshee ]
+      elif [[ ${oynatici} = banshee ]]
       then
           banshee --pause > /dev/null 2>&1
           DURDURULAN+=('banshee')
-      elif [ ${oynatici} = exaile ]
+      elif [[ ${oynatici} = exaile ]]
       then
           exaile --play-pause > /dev/null 2>&1
           DURDURULAN+=('exaile')
-      elif [ ${oynatici} = moc ]
+      elif [[ ${oynatici} = moc ]]
       then
           moc --pause > /dev/null 2>&1
           DURDURULAN+=('moc')
-      elif [ ${oynatici} = cmus ]
+      elif [[ ${oynatici} = cmus ]]
       then
           if [[ $(cmus-remote -C status | head -1) = status\ playing ]]
           then
               cmus-remote --pause > /dev/null 2>&1
               DURDURULAN+=('cmus')
           fi
-      elif [ ${oynatici} = qmmp ]
+      elif [[ ${oynatici} = qmmp ]]
       then
           if qdbus_sorgu qmmp;
           then
@@ -106,40 +106,41 @@ function oynatici_islem() {
   done
 
 
-  ezandinlet
+  mplayer_calistir "${vakit_ezani}"
+  (( EZAN_DUASI_OKU )) && mplayer_calistir "${EZAN_DUASI}"
   for oynatici in ${DURDURULAN[@]}
   do
-      if [ ${oynatici} = deadbeef ]
+      if [[ ${oynatici} = deadbeef ]]
       then
           deadbeef --play > /dev/null 2>&1
-      elif [ ${oynatici} = clementine ]
+      elif [[ ${oynatici} = clementine ]]
       then
           clementine --play > /dev/null 2>&1
-      elif [ ${oynatici} = amarok ]
+      elif [[ ${oynatici} = amarok ]]
       then
           amarok --play > /dev/null 2>&1
-      elif [ ${oynatici} = rhythmbox ]
+      elif [[ ${oynatici} = rhythmbox ]]
       then
           rhythmbox-client --play > /dev/null 2>&1
-      elif [ ${oynatici} = aqulung ]
+      elif [[ ${oynatici} = aqulung ]]
       then
           aqualung --play > /dev/null 2>&1
-      elif [ ${oynatici} = audacious ]
+      elif [[ ${oynatici} = audacious ]]
       then
           audacious --play > /dev/null 2>&1
-      elif [ ${oynatici} = banshee ]
+      elif [[ ${oynatici} = banshee ]]
       then
           banshee --play > /dev/null 2>&1
-      elif [ ${oynatici} = exaile ]
+      elif [[ ${oynatici} = exaile ]]
       then
           exaile --play-pause > /dev/null 2>&1
-      elif [ ${oynatici} = moc ]
+      elif [[ ${oynatici} = moc ]]
       then
           moc --play > /dev/null 2>&1
-      elif [ ${oynatici} = cmus ]
+      elif [[ ${oynatici} = cmus ]]
       then
           cmus-remote --play > /dev/null 2>&1
-      elif [ ${oynatici} = qmmp ]
+      elif [[ ${oynatici} = qmmp ]]
       then
           qmmp --play > /dev/null 2>&1
       fi
