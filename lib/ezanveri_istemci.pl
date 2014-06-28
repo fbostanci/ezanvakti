22421fe4b15cfcb5e9f896eaa1ffe17625236397
ironic <faopera@gmail.com>
2014-06-28 12:38:22 +0300

    copyright ..

--- lib/ezanveri_istemci.pl
+++ lib/ezanveri_istemci.pl
@@ -8,12 +8,18 @@
 ##                   https://gitorious.org/ezanvakti
 ##
 ##
-##    Bu uygulama bir özgür yazılımdır: yeniden dağıtabilirsiniz ve/veya
-##    Özgür Yazılım Vakfı  (FSF) tarafından yayımlanan  (GPL) Genel kamu
-##    lisansı sürüm 3  veya daha  yeni bir sürümünde belirtilen şartlara
-##    uymak kaydıyla, üzerinde değişiklik yapabilirsiniz.
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
 ##
-##    Ayrıntılar için COPYING dosyasını okuyun.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see http://www.gnu.org/licenses/.
 #
 #
 
