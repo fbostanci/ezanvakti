0fbc0178be82eb81c1238b2479d825e65ba2ed8e
ironic <faopera@gmail.com>
2014-08-10 13:07:38 +0300

    duzeltmeler 14 * conky-iftar

--- lib/conky.bash
+++ lib/conky.bash
@@ -14,13 +14,18 @@ function ezv_conky() {
 function ezv_conky_iftar() {
   denetle; bugun
 
-  [[ $UNIXSAAT -lt $aksam ]] && {
+  (( UNIXSAAT < aksam )) && {
     bekleme_suresi $aksam_n; kalan
     echo -e "İftar : $aksam_n\nKalan : $kalan_sure" |
       sed 's:saat:sa:;s:dakika:dk:;s:saniye:sn:'
   } || {
-    [[ $UNIXSAAT -ge $aksam ]] && {
-      # FIXME: Yarının aksam vakti ezanveri dosyasında var mı denetle önc..
+    (( UNIXSAAT >= aksam )) && {
+      # Yarının aksam vakti ezanveri dosyasında var mı denetle önc..
+      [[ -z $(grep $(date -d 'tomorrow' +%d.%m.%Y) "${EZANVERI}") ]] && {
+        (( GUNCELLEME_YAP )) && { bilesen_yukle guncelleyici; guncelleme_yap;} || {
+          printf "${EZANVERI_ADI} dosyanızda yarına ait veri bulunmuyor.\n"; exit 1
+        }
+      }
       export $(gawk '{printf "aksam_n=%s", $6}' \
         <(grep $(date -d 'tomorrow' +%d.%m.%Y) "${EZANVERI}"))
       bekleme_suresi_yarin $aksam_n; kalan
