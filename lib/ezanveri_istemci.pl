a9881a6f654fe97149ee7a7ceb94e201e2be71d8
fbostanci <ironic@yaani.com>
2015-01-11 02:58:50 +0200

    makefile gelistirmesi

--- lib/ezanveri_istemci.pl
+++ lib/ezanveri_istemci.pl
@@ -3,7 +3,7 @@
 #                          Ezanveri İstemci 2.0
 #
 ##
-##          Copyright (c) 2010-2014 Fatih Bostancı  <faopera@gmail.com>
+##          Copyright (c) 2010-2015 Fatih Bostancı  <faopera@gmail.com>
 ##
 ##                   https://gitorious.org/ezanvakti
 ##
