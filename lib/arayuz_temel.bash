a36ffd76fa6747ddfc5d2a411d1ed9524dc2f32a
ironic <faopera@gmail.com>
2017-01-28 16:22:30 +0300

    arayüz geliştirme 3

--- lib/arayuz_temel.bash
+++ lib/arayuz_temel.bash
@@ -4,17 +4,20 @@
 #
 #
 
-sure_listesi='!1-Fatiha!2-Bakara!3-Al-i İmran!4-Nisa!5-Maide!6-Enam!7-Araf!8-Enfal!9-Tevbe!10-Yunus\
-!11-Hud!12-Yusuf!13-Rad!14-İbrahim!15-Hicr!16-Nahl!17-İsra!18-Kehf!19-Meryem!20-Taha!21-Enbiya!22-Hac\
-!23-Müminun!24-Nur!25-Furkan!26-Şuara!27-Neml!28-Kasas!29-Ankebut!30-Rum!31-Lokman!32-Secde!33-Ahzab\
-!34-Sebe!35-Fatır!36-Yasin!37-Saffat!38-Sad!39-Zümer!40-Mümin!41-Fussilet!42-Şura!43-Zuhruf!44-Duhan\
-!45-Casiye!46-Akaf!47-Muhammed!48-Fetih!49-Hucurat!50-Kaf!51-Zariyat!52-Tur!53-Necm!54-Kamer!55-Rahman\
-!56-Vakia!57-Hadid!58-Mücadele!59-Haşr!60-Mümtehine!61-Saf!62-Cuma!63-Münafikun!64-Tegabun!65-Talak\
-!66-Tahrim!67-Mülk!68-Kalem!69-Hakka!70-Mearic!71-Nuh!72-Cin!73-Müzzemmil!74-Müddessir!75-Kıyame\
-!76-İnsan!77-Mürselat!78-Nebe!79-Naziat!80-Abese!81-Tekvir!82-İnfitar!83-Mutaffifın!84-İnşikak\
-!85-Büruc!86-Tarık!87-Ala!88-Gaşiye!89-Fecr!90-Beled!91-Şems!92-Leyl!93-Duha!94-İnşirah!95-Tin\
-!96-Alak!97-Kadir!98-Beyyine!99-Zilzal!100-Adiyat!101-Karia!102-Tekasür!103-Asr!104-Hümeze\
-!105-Fil!106-Kureyş!107-Maun!108-Kevser!109-Kafirun!110-Nasr!111-Tebbet!112-İhlas!113-Felak!114-Nas'
+sure_listesi='!1-Fatiha!2-Bakara!3-Al-i İmran!4-Nisa!5-Maide!6-Enam!7-Araf!8-Enfal!9-Tevbe!10-Yunus'
+sure_listesi+='!11-Hud!12-Yusuf!13-Rad!14-İbrahim!15-Hicr!16-Nahl!17-İsra!18-Kehf!19-Meryem!20-Taha'
+sure_listesi+='!21-Enbiya!22-Hac!23-Müminun!24-Nur!25-Furkan!26-Şuara!27-Neml!28-Kasas!29-Ankebut'
+sure_listesi+='!30-Rum!31-Lokman!32-Secde!33-Ahzab!34-Sebe!35-Fatır!36-Yasin!37-Saffat!38-Sad'
+sure_listesi+='!39-Zümer!40-Mümin!41-Fussilet!42-Şura!43-Zuhruf!44-Duhan!45-Casiye!46-Akaf'
+sure_listesi+='!47-Muhammed!48-Fetih!49-Hucurat!50-Kaf!51-Zariyat!52-Tur!53-Necm!54-Kamer'
+sure_listesi+='!55-Rahman!56-Vakia!57-Hadid!58-Mücadele!59-Haşr!60-Mümtehine!61-Saf!62-Cuma'
+sure_listesi+='!63-Münafikun!64-Tegabun!65-Talak!66-Tahrim!67-Mülk!68-Kalem!69-Hakka!70-Mearic'
+sure_listesi+='!71-Nuh!72-Cin!73-Müzzemmil!74-Müddessir!75-Kıyame!76-İnsan!77-Mürselat!78-Nebe'
+sure_listesi+='!79-Naziat!80-Abese!81-Tekvir!82-İnfitar!83-Mutaffifın!84-İnşikak!85-Büruc'
+sure_listesi+='!86-Tarık!87-Ala!88-Gaşiye!89-Fecr!90-Beled!91-Şems!92-Leyl!93-Duha!94-İnşirah'
+sure_listesi+='!95-Tin!96-Alak!97-Kadir!98-Beyyine!99-Zilzal!100-Adiyat!101-Karia!102-Tekasür'
+sure_listesi+='!103-Asr!104-Hümeze!105-Fil!106-Kureyş!107-Maun!108-Kevser!109-Kafirun!110-Nasr'
+sure_listesi+='!111-Tebbet!112-İhlas!113-Felak!114-Nas'
 
 function g_vakitleri_al() {
   denetle; bugun
@@ -77,7 +80,7 @@ function g_vakitleri_yaz() {
 }
 
 function g_secim_goster() {
-  yad --title "Ezanvakti $SURUM" --text-info --filename=/tmp/ezanvakti-6 --width=420 --height=300 --wrap \
+  yad --title "Ezanvakti $SURUM - ${secim_basligi}" --text-info --filename=/tmp/ezanvakti-6 --width=560 --height=300 --wrap \
       --button='gtk-close' --window-icon=${VERI_DIZINI}/simgeler/ezanvakti2.png --back="$ARKAPLAN_RENGI"\
       --fore="$YAZI_RENGI" --mouse --sticky --tail
 }
@@ -86,7 +89,7 @@ function g_hakkinda() {
   yad --text "\t\t<b><big>Ezanvakti ${SURUM}</big></b>
 \"GNU/Linux için ezan vakti bildirici\"
 
-   <a href= 'https://gitlab.com/ironic/ezanvakti' >https://gitlab.com/ironic/ezanvakti</a>
+   <a href= 'https://gitlab.com/ironic/ezanvakti' >Ezanvakti Sayfası</a>
 
 Copyright (c) 2010-2017 Fatih Bostancı
 GPL 3 ile lisanslanmıştır.\n" \
@@ -104,8 +107,8 @@ yad --form --separator=' ' --title="Ezanvakti $SURUM" --text "${mplayer_ileti}" 
   --button='gtk-cancel:127' --button='gtk-close:0' --sticky --mouse --fixed
 
   case $? in
-    127)
-      echo stop > /tmp/mplayer-$$.pipe 2>/dev/null;sleep .4
+    *)
+      echo stop > /tmp/mplayer-$$.pipe 2>/dev/null
       echo stop > /tmp/mplayer-$$.pipe2 2>/dev/null
       rm -f /tmp/mplayer-$$.pipe{,2} &>/dev/null
       ;;
@@ -135,20 +138,28 @@ case $donus in
     [[ -z $str2 ]] && ozel_pencere
     sure=$(cut -d'-' -f1 <<<"$str2")
 
-     if [ "${str}" = "Saad el Ghamdi" ]
-     then
+    if [[ ${str} = Saad el Ghamdi ]]
+    then
         okuyucu=AlGhamdi
-     elif [ "${str}" = "As Shatry" ]
-     then
+    elif [[ ${str} = As Shatry ]]
+    then
         okuyucu=AsShatree
-     elif [ "${str}" = "Ahmad el Ajmy" ]
-     then
+    elif [[ ${str} = Ahmad el Ajmy ]]
+    then
         okuyucu=AlAjmy
-     else
+    else
         okuyucu='Yerel Okuyucu'
-     fi
+    fi
 
-    if [ "${okuyucu}" = "Yerel Okuyucu" ]
+    if (( ${#sure} == 1 ))
+    then
+        sure=00$sure
+    elif (( ${#sure} == 2 ))
+    then
+        sure=0$sure
+    fi
+
+    if [[ ${okuyucu} = Yerel Okuyucu ]]
     then
         if [[ -f "${YEREL_SURE_DIZINI}/$sure.mp3" ]]
         then
@@ -159,14 +170,11 @@ case $donus in
     else
         dinletilecek_sure="http://www.listen2quran.com/listen/${okuyucu}/$sure.mp3"
     fi
-    rm -f /tmp/mplayer-$$.pipe 2>/dev/null
-    mkfifo /tmp/mplayer-$$.pipe
-    mplayer_ileti="$(grep -w $sure ${VERI_DIZINI}/veriler/sureler | gawk '{print $2}') suresi dinletiliyor..\
+
+    mplayer_ileti="$(gawk -v sira=$sure '{if(NR==sira) print $4;}' < ${VERI_DIZINI}/veriler/sure_bilgisi) suresi dinletiliyor..\
     \n\n Okuyan : ${str}"
-    pencere_bilgi &
-    $MPLAYER -slave -input file=/tmp/mplayer-$$.pipe "${dinletilecek_sure}" 2> /dev/null; clear
-    pkill yad &>/dev/null
-    ozel_pencere;;
+    mplayer_calistir "${dinletilecek_sure}" &
+    pencere_bilgi; ozel_pencere ;;
   153)
     exit 0 ;;
 esac
