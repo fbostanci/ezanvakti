1adfcab10512f2ea07dc9f258030d89ab492c0d9
fbostanci <faopera@gmail.com>
2017-07-06 17:23:13 +0300

    unity menu güncellendi.

---
+++ lib/unity_menu.bash
@@ -0,0 +1,86 @@
+#
+#
+#
+#
+#
+
+bilesen_yukle arayuz_temel
+
+function unity_pid_denetle() {
+  local ypid=$$
+
+  if [[ -f /tmp/.ezanvakti_unity_menu.pid && -n $(ps -p $( < /tmp/.ezanvakti_unity_menu.pid) -o comm=) ]]
+  then
+      printf "Yalnızca bir arayüz örneği çalışabilir.\n" >&2
+      exit 1
+  else
+      printf "$ypid" > /tmp/.ezanvakti_unity_menu.pid
+  fi
+}
+
+function unity_menu() {
+  unity_pid_denetle
+
+  case $1 in
+    vakitler)
+      secim_basligi='Vakitler'
+      bilesen_yukle vakitleri_goster
+      vakitler tum_vakitler > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    kerahat)
+      secim_basligi='Kerahat Vakitleri'
+      bilesen_yukle kerahat
+      kerahat_vakitleri ucbirim > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    kuran)
+      bilesen_yukle mplayer_yonetici
+      ozel_pencere ;;
+
+    gunler)
+      secim_basligi="$(date +%Y) Dini Günler ve Geceler"
+      bilesen_yukle dini_gunler
+      gunler > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    ayet)
+      secim_basligi='Günlük Ayet'
+      bilesen_yukle ayet_goster
+      ayet_goster ucbirim > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    hadis)
+      secim_basligi='40 Hadis'
+      bilesen_yukle bilgi_goster
+      hadis_goster ucbirim > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    bilgi)
+      secim_basligi='Dini Bilgiler'
+      bilesen_yukle bilgi_goster
+      bilgi_goster ucbirim > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    esma)
+      secim_basligi='Esma-ül Hüsna'
+      bilesen_yukle bilgi_goster
+      esma_goster ucbirim > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    yapilandirma)
+      bilesen_yukle yapilandirma_yoneticisi
+      yapilandirma ;;
+
+    yardim)
+      secim_basligi='Kullanım'
+      bilesen_yukle kullanim
+      betik_kullanimi > /tmp/ezanvakti-6
+      g_secim_goster ;;
+
+    hakkinda)
+      g_hakkinda ;;
+
+  esac
+}
+
