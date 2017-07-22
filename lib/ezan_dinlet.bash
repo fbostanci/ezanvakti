5160d9d88e411590550fd57c90ff9237f59d8205
fbostanci <faopera@gmail.com>
2017-07-22 13:06:01 +0300

    duzenlemeler

--- lib/ezan_dinlet.bash
+++ lib/ezan_dinlet.bash
@@ -14,7 +14,7 @@ ezan_dinlet() {
 
   bilesen_yukle mplayer_yonetici
 
-  printf "${RENK7}${RENK2}${vakit}${RENK3} ${ileti}dinletiliyor...\n\n${RENK0}"
+  printf "${RENK7}${RENK2}${vakit}${RENK3} ${ileti}dinletiliyor\n\n${RENK0}"
   printf "${RENK7}${RENK3}Okuyan : ${RENK2}${EZAN_OKUYAN}\n\n${RENK0}"
   mplayer_calistir "${vakit_ezani}"
 }
