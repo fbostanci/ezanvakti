6ae8fe46b36327d30b1833065ddad682a1a33c08
fbostanci <ironic@yaani.com>
2020-05-04 04:03:15 +0300

    bilgi_goster.bash, temel_islevler.bash  kod temizliği, geliştirme

--- lib/bilgi_goster.bash
+++ lib/bilgi_goster.bash
@@ -14,21 +14,24 @@ secim_yap() {
   alinan_yanit="$(sed -n "/#$secilen<#/,/#>$secilen#/p" ${icerik_al} | sed '1d;$d')"
 }
 
+ucbirimde_goster() {
+  if (( ! ${RENK:-RENK_KULLAN} ))
+  then
+      printf "* ${alinan_yanit}\n"
+  else
+      gawk -v r0=${RENK0} -v r3=${RENK3} -v r7=${RENK7} -v r7=${RENK8} \
+        '{if (NR==1) {printf "%s%s%s%s\n",r7,r3,$0,r0;} \
+          else if (NR>1) {printf "%s%s%s%s\n",r7,r8,$0,r0;} }' <<< "${alinan_yanit}"
+  fi
+}
+
 bilgi_goster() {
   icerik_al=${VERI_DIZINI}/veriler/bilgiler
   secim_yap 157
 
   case $1 in
     ucbirim)
-      if (( ! ${RENK:-RENK_KULLAN} ))
-      then
-          printf "*${alinan_yanit}\n"
-      else
-          gawk -v r0=${RENK0} -v r3=${RENK3} -v r7=${RENK7} -v r7=${RENK8} \
-            '{if (NR==1) {printf "%s%s%s%s\n",r7,r3,$0,r0;} \
-              else if (NR>1) {printf "%s%s%s%s\n",r7,r8,$0,r0;} }' <<< "${alinan_yanit}"
-      fi ;;
-
+      ucbirimde_goster ;;
     bildirim)
       notify-send "Bunları biliyor musunuz?" "$(printf "*${alinan_yanit}\n")" \
         -t $BILGI_BILDIRIM_SURESI"000" ;;
@@ -41,15 +44,7 @@ hadis_goster() {
 
   case $1 in
     ucbirim)
-      if (( ! ${RENK:-RENK_KULLAN} ))
-      then
-          printf "${alinan_yanit}\n"
-      else
-          gawk -v r0=${RENK0} -v r3=${RENK3} -v r7=${RENK7} -v r7=${RENK8} \
-            '{if (NR==1) {printf "%s%s%s%s\n",r7,r3,$0,r0;} \
-              else if (NR>1) {printf "%s%s%s%s\n",r7,r8,$0,r0;} }' <<< "${alinan_yanit}"
-      fi ;;
-
+      ucbirimde_goster ;;
     bildirim)
       notify-send "$secilen. hadis" "$(sed '1d' <<<"${alinan_yanit}")" \
         -t $HADIS_BILDIRIM_SURESI"000" ;;
@@ -59,15 +54,7 @@ hadis_goster() {
 esma_goster() {
   icerik_al=${VERI_DIZINI}/veriler/esma
   secim_yap 99
-
-  if (( ! ${RENK:-RENK_KULLAN} ))
-  then
-      printf "${alinan_yanit}\n"
-  else
-      gawk -v r0=${RENK0} -v r3=${RENK3} -v r7=${RENK7} -v r7=${RENK8} \
-        '{if (NR==1) {printf "%s%s%s%s\n",r7,r3,$0,r0;} \
-          else if (NR>1) {printf "%s%s%s%s\n",r7,r8,$0,r0;} }' <<< "${alinan_yanit}"
-  fi
+  ucbirimde_goster
 }
 
 # vim: set ft=sh ts=2 sw=2 et:
